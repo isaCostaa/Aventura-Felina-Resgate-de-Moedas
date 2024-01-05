d196b66804868c4f52c153e75b5a7fbc9f5a5559
Isabella Costa <104528140+isaCostaa@users.noreply.github.com>
2024-01-04 21:03:07 -0300

    criando animaçao do player

--- Aventura_Felina/Assets/Scripts/Player.cs
+++ Aventura_Felina/Assets/Scripts/Player.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Animator playerAnim;
     public float velocity = 5f; // Velocidade de movimento do personagem
     public float jump = 10f; // Força do pulo
     private Rigidbody2D rb;
     private bool ground; // Verifica se o personagem está no chão
+    private SpriteRenderer sr; 
+    
 
     void Start()
     {
+        playerAnim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>(); // faz com que o player vire 
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         if (Input.GetButtonDown("Jump"))
-        {
+        {   
+            playerAnim.SetBool("jump", true);
             rb.velocity = Vector2.up * 12;
         }
 
 
         // Verifica se o personagem está no chão
         ground = Physics2D.OverlapCircle(transform.position, 0.2f, LayerMask.GetMask("ground"));
+        
 
         // Movimentação horizontal
         float movimentHorizontal = Input.GetAxis("Horizontal");
@@ -33,8 +40,35 @@ public class Player : MonoBehaviour
         // Pulo
         if (ground && Input.GetKeyDown(KeyCode.W))
         {
+            
             rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+            
         }
 
+    
+
+        if (movimentHorizontal > 0) // acessando o sprite render do inspector
+        {
+            playerAnim.SetBool("walk", true);
+            sr.flipX = false;
+        }
+
+
+        else if (movimentHorizontal < 0) // acessando o sprite render do inspector
+        {
+            playerAnim.SetBool("walk", true);
+            sr.flipX = true;
+        }
+
+        else 
+        {
+            playerAnim.SetBool("walk", false);
+  
+        }
+
+        
+
+        
+        
     }
 }
