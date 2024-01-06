54fd956a29c9b3b3f3163a63e75bba215ada3ff6
Isabella Costa <104528140+isaCostaa@users.noreply.github.com>
2024-01-05 23:54:10 -0300

    att

--- Aventura_Felina/Assets/Scripts/Player.cs
+++ Aventura_Felina/Assets/Scripts/Player.cs
@@ -10,6 +10,11 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private bool ground; // Verifica se o personagem está no chão
     private SpriteRenderer sr; 
+
+  
+
+
+
     
 
     void Start()
@@ -21,33 +26,49 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+
+         // Verifica se o personagem está no chão
+        ground = Physics2D.OverlapCircle(transform.position, 0.2f, LayerMask.GetMask("ground"));
+       
+
+
         if (Input.GetButtonDown("Jump"))
         {   
-            playerAnim.SetBool("jump", true);
+           
             rb.velocity = Vector2.up * 12;
+            
+        }
+        
+
+         // Pulo
+        if (ground && Input.GetKeyDown(KeyCode.W))
+        {
+            
+            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+            playerAnim.SetBool("jump", true);
         }
 
 
-        // Verifica se o personagem está no chão
-        ground = Physics2D.OverlapCircle(transform.position, 0.2f, LayerMask.GetMask("ground"));
-        
+        if (ground && rb.velocity.y ==0)
+        {
+            //pulosExtras = 1;
+            playerAnim.SetBool("jump", false);
+
+        }
+
+
+     
+
 
         // Movimentação horizontal
         float movimentHorizontal = Input.GetAxis("Horizontal");
         Vector2 moviment = new Vector2(movimentHorizontal * velocity, rb.velocity.y);
         rb.velocity = moviment;
 
-        // Pulo
-        if (ground && Input.GetKeyDown(KeyCode.W))
-        {
-            
-            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
-            
-        }
 
     
 
-        if (movimentHorizontal > 0) // acessando o sprite render do inspector
+         if (movimentHorizontal > 0) // acessando o sprite render do inspector
         {
             playerAnim.SetBool("walk", true);
             sr.flipX = false;
@@ -67,8 +88,6 @@ public class Player : MonoBehaviour
         }
 
         
-
-        
-        
+      
     }
 }
